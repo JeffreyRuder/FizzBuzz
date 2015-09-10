02d8db86d3c7cdcd14f6c000190efbebade1d5c8
Jeffrey Ruder <ruderjt@gmail.com>
2015-09-10 11:15:06 -0700

    Replaced document.write() with DOM manipulation.

--- JavaScript.js
+++ JavaScript.js
@@ -1,20 +1,20 @@
 var numToPingPong = prompt("To which number do you wish to ping-pong? Please select a positive non-zero integer.");
 
 for (ii = 1; ii <= numToPingPong; ii++) {
+    var output = ""
     if((ii % 3 === 0) && (ii % 5 === 0)) {
-        document.write("Ping-Pong");
-        document.write("<br>");
+        output = "Ping-Pong";
     }
     else if(ii % 3 === 0) {
-        document.write("Ping");
-        document.write("<br>");
+        output = "Ping";
     }
     else if(ii % 5 === 0) {
-        document.write("Pong");
-        document.write("<br>");
+        output = "Pong";
     }
     else {
-        document.write(ii);
-        document.write("<br>");
+        output = ii;
     }
+    
+    //Assumes that the document has a #pingPongList element.
+    document.getElementById('pingPongList').innerHTML += '<li>' + output + '</li>';
 }
