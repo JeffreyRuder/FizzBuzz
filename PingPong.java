0c2aa9b377c19749f368de0f77bec316530317cc
Jeffrey Ruder <ruderjt@gmail.com>
2015-09-28 21:40:25 -0700

    Added command line argument to set PingPong number.

--- PingPong.java
+++ PingPong.java
@@ -1,18 +1,35 @@
 public class PingPong {
 	public static void main(String[] args) {
-			for (int ii = 1; ii < 101; ii++) {
-				if (ii % 15 == 0) {
-					System.out.println("PingPong");
-				}
-				else if (ii % 3 == 0) {
-					System.out.println("Ping");
-				}
-				else if (ii % 5 == 0) {
-					System.out.println("Pong");
-				}
-				else {
-					System.out.println(ii);
-				}
+
+		int firstArg;
+
+		if (args.length == 0) {
+			System.out.println("Please enter a number to PingPong to.");
+			System.exit(0);
+		}
+
+		if (args.length > 0) {
+			try {
+				firstArg = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				System.err.println("Number to PingPong to must be an integer, but you entered " + args[0]);
+				System.exit(1);
 			}
+		}
+		
+		for (int ii = 1; ii < args[0]; ii++) {
+			if (ii % 15 == 0) {
+				System.out.println("PingPong");
+			}
+			else if (ii % 3 == 0) {
+				System.out.println("Ping");
+			}
+			else if (ii % 5 == 0) {
+				System.out.println("Pong");
+			}
+			else {
+				System.out.println(ii);
+			}
+		}
 	}
 }
